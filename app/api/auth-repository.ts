20976f079f85b054cd48373dffdab5668d306843
Gvozdoslav <remrostrus2123@yandex.ru>
2024-06-28 02:30:57 +0300

    feat: implement login and register methods to use lms backend api

---
+++ app/api/auth-repository.ts
@@ -0,0 +1,53 @@
+import api from "@/app/api/index";
+import {cookies} from "next/headers";
+
+
+export type LoginForm = {
+    email: string,
+    password: string,
+}
+
+
+export const login = async ({email, password}: LoginForm) => {
+    try {
+        const response = await api.post('/auth/login', {
+            headers: {
+                email: email,
+                password: password,
+            }
+        });
+        const token = response.headers['authorization'] || response.headers['Authorization'];
+
+        if (token) {
+            const tokenValue = token.startsWith('Bearer ') ? token.slice(7) : token;
+            const expirationTime = new Date(new Date().getTime() + 60 * 60 * 1000);
+            cookies().set('token', tokenValue, { expires:  expirationTime});
+        }
+    } catch (error) {
+        console.log(error);
+        throw error
+    }
+}
+
+
+export type RegisterForm = {
+    name: string,
+    role?: string | null
+}
+
+const defaultRegisterForm = {
+    name: "",
+    role: "USER",
+}
+
+
+export const register = async (registerForm: Partial<RegisterForm>, email: string, password: string) => api.post('/auth/login', {
+    headers: {
+        email: email,
+        password: password,
+    },
+    body: {
+        ...defaultRegisterForm,
+        ...registerForm
+    },
+})
